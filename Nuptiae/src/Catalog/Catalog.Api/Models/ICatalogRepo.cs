4607eca7316df2af33d90dde34c20828f383fd28
Louisa <rangbaastrid@gmail.com>
2020-12-17 00:10:51 +0100

    add comment on interface

--- Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
+++ Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
@@ -12,8 +12,13 @@ namespace Catalog.Api.Models
         /// </summary>
         /// <param name="pageSize"></param>
         /// <param name="pageNum"></param>
-        /// <returns></returns>
+        /// <returns>All travels</returns>
         IEnumerable<CatalogTravel> GetTravel(int pageSize, int pageNum);
+        /// <summary>
+        /// Get travels by id 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>one travels</returns>
         CatalogTravel GetTravelById(int id);
 
     }
