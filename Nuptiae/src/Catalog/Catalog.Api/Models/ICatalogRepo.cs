b2535b5d80d44aa59819ce0c0d064ea609213cef
Louisa <rangbaastrid@gmail.com>
2020-12-17 00:05:22 +0100

    add comment in Idisposable class

--- Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
+++ Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
@@ -7,6 +7,12 @@ namespace Catalog.Api.Models
 {
     public interface ICatalogRepo : IDisposable
     {
+        /// <summary>
+        /// Get travels information
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNum"></param>
+        /// <returns></returns>
         IEnumerable<CatalogTravel> GetTravel(int pageSize, int pageNum);
         CatalogTravel GetTravelById(int id);
 
