b65edc80fe45d4a9a51d6ef0a625ead49e935139
Louisa <rangbaastrid@gmail.com>
2021-02-13 15:01:27 +0100

    Add method findtravelbycountry return all travels search name

--- Nuptiae/src/Catalog/Catalog.Api/Controllers/CatalogController.cs
+++ Nuptiae/src/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Catalog.API.Controllers
 {
@@ -11,17 +12,16 @@ namespace Catalog.API.Controllers
     [Route("api/v1/[controller]")]
     public class CatalogController : ControllerBase
     {
-
         private readonly ILogger<CatalogController> _logger;
         private readonly ICatalogRepo _repo;
 
+        //public CatalogController ():this(new ICatalogRepo(),new ILogger<CatalogController>) { }
         public CatalogController(ICatalogRepo repo, ILogger<CatalogController> logger)
         {
             _logger = logger;
             _repo = repo;
         }
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
         /// <summary>
         /// Get a paginated list of the catalog destinations
         /// </summary>
@@ -31,19 +31,20 @@ namespace Catalog.API.Controllers
         /// <response code="200">Request successfully processed</response>
         /// <response code="400">Error in the request parameters</response>
         [HttpGet("travel")]
-        public ActionResult<IEnumerable<CatalogTravel>> Get([FromQuery] int pageNum = 0, [FromQuery] int pageSize = 10)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogTravel>>> Get([FromQuery] int pageNum = 0, [FromQuery] int pageSize = 10)
         {
             try
             {
-                return Ok(_repo.GetTravel(pageSize, pageNum));
+                return Ok(await _repo.GetTravel(pageSize, pageNum));
             }
             catch (ArgumentOutOfRangeException e)
             {
                 return BadRequest(e.Message);
             }
         }
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
         /// <summary>
         /// get the specified destination from his id 
         /// </summary>
@@ -52,9 +53,11 @@ namespace Catalog.API.Controllers
         /// <response code="200">Catalog Item with the given ID found</response>
         /// <response code="404">No catalog item with the given ID found</response>
         [HttpGet("travel/{id:int}")]
-        public ActionResult<CatalogTravel> Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CatalogTravel>> Get(int id)
         {
-            var result = _repo.GetTravelById(id);
+            var result = await _repo.GetTravelById(id);
             if (result == null)
             {
                 return NotFound();
@@ -65,11 +68,31 @@ namespace Catalog.API.Controllers
             }
 
         }
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+
         /// <summary>
-        /// delete the specified destination from his Id 
+        /// get the specified destination from his country 
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns>Catalog Destnations found </returns>
+        /// <response code="200">Catalog Item with the given ID found</response>
+        /// <response code="404">No catalog item with the given ID found</response>
+        [HttpGet("travel/{search}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CatalogTravel>> Get(string search)
+        {
+            var result = await _repo.FindTravelByCountry(search);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return result;
+
+        }
+
+        /// <summary>
+        /// delete a destination with given id 
         /// </summary>
         /// <param name="id"></param>
         /// <returns>" "</returns>
@@ -77,13 +100,40 @@ namespace Catalog.API.Controllers
         /// <response code="204">Success no content</response>
         /// <response code="404">No catalog item with the given ID found</response>
         [HttpDelete("travel/{id:int}")]
-        public ActionResult Delete(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete([FromRoute] int id)
         {
-            CatalogTravel result = _repo.GetTravelById(id);
+            CatalogTravel result = await _repo.GetTravelById(id);
             if (result == null)
+            {
                 return NotFound("unKnow travel");
-            else
-                return NoContent(); 
+            }
+            _repo.RemoveTavel(id);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Search with Country return all country travel by name 
+        /// </summary>
+        /// <param name="country"></param>
+        /// <param name="pageNum"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        [HttpGet("trip/{search:regex(^[[a-zA-Z]])}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogTravel>>> Get([FromQuery] string search, [FromQuery] int pageNum = 0, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                return Ok(await _repo.FindTravelsByCountry(search, pageSize, pageNum));
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
--- Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
+++ Nuptiae/src/Catalog/Catalog.Api/Models/ICatalogRepo.cs
@@ -7,19 +7,47 @@ namespace Catalog.Api.Models
 {
     public interface ICatalogRepo : IDisposable
     {
+
         /// <summary>
         /// Get travels information
         /// </summary>
         /// <param name="pageSize"></param>
         /// <param name="pageNum"></param>
         /// <returns>All travels</returns>
-        IEnumerable<CatalogTravel> GetTravel(int pageSize, int pageNum);
+        Task<IEnumerable<CatalogTravel>> GetTravel(int pageSize, int pageNum);
         /// <summary>
         /// Get travels by id 
         /// </summary>
         /// <param name="id"></param>
         /// <returns>one travels</returns>
-        CatalogTravel GetTravelById(int id);
+        Task<CatalogTravel> GetTravelById(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        Task<CatalogTravel> FindTravelByCountry(string search);
+
+
+        void RemoveTavel(int id);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNum"></param>
+        /// <returns></returns>
+        Task<IEnumerable<CatalogTravel>> FindTravelsByCountry(string search, int pageSize, int pageNum);
+
+        ///// <summary>
+        ///// Add a new Travel 
+        ///// </summary>
+        ///// <param name="newTravel"></param>
+        ///// <returns>A new travel added </returns>
+        //CatalogTravel AddNewTravel(CatalogTravel newTravel);
 
     }
 }
